9cbe350148430a9c5751cacc368df8fcca38cc98
Yasmin naomi <yasminmin1704@gmail.com>
2024-06-06 03:34:08 -0300

    Add local section and fix font text

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(-${currentSlide * 100}%)`;
+    });
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Initialize the slider
+showSlide(currentSlide);
